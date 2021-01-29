114fe67ae0eafb4d79990ac380beef0d55e031a0
JoseRamosAzofeifa <jara.zff@gmail.com>
2021-01-29 18:32:27 +0000

    Domain Name Generator

---
+++ app.js
@@ -0,0 +1,21 @@
+let pronoun = ['the', 'our'];
+let adj = ['great', 'big'];
+let noun = ['jogger', 'racoon'];
+let dominio =[".com",".net",".com",".us",".io"];
+
+for (i = 0; i < pronoun.length; i++) {
+
+    for (j = 0; j < adj.length; j++) {
+
+        for (k = 0; k < noun.length; k++) {
+
+            for (l = 0; l < dominio.length; l++) {
+           
+                console.log(`${pronoun[i]}${adj[j]}${noun[k]}${dominio[l]}`);
+                }
+
+        }
+    }
+
+
+}
